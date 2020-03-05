1683f966b1df277136f0afb012809b882e82a45d
linson <linson@linsondemac.local>
2020-03-05 15:26:52 +0800

    Initial Commit

---
+++ LearnObjective-c/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LearnObjective-c
+//
+//  Created by linson on 2020/3/5.
+//  Copyright © 2020年 linson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
